b23523bfea83e561a11774f32616d6fa1416d053
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-08-01 23:35:54 +0200

    New test function

--- MyPythonProject/Applications/AudioFiles/Modules/shared.py
+++ MyPythonProject/Applications/AudioFiles/Modules/shared.py
@@ -1,5 +1,6 @@
 # -*- coding: ISO-8859-1 -*-
 from collections import namedtuple
+import re
 
 __author__ = 'Xavier ROSSET'
 
@@ -10,3 +11,25 @@ def grabdiscnumber(fil, rex):
     if match:
         return Disc(True, match.group(1))
     return Disc(False, None)
+
+
+def getfileindex(index, lst):
+    l = []
+    rex1 = re.compile(r"^\d{1,2}$")
+    rex2 = re.compile(r"^(\d{1,2})\-(\d{1,2})$")
+    for i in index.split(", "):
+        match = False
+        if not match:
+            match = rex1.match(i)
+            if match:
+                i = int(i)
+                if i <= len(lst):
+                    l.append(i)
+        if not match:
+            match = rex2.match(i)
+            if match:
+                if int(match.group(2)) >= int(match.group(1)):
+                    for j in range(int(match.group(1)), int(match.group(2))+1):
+                        if j <= len(lst):
+                            l.append(j)
+    return sorted(l)
