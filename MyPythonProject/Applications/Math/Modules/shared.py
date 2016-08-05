0b4c44e8c71844989a20a4cf097141696b2c9d9b
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-08-05 17:24:01 +0200

    Use property decorator

--- MyPythonProject/Applications/Math/Modules/shared.py
+++ MyPythonProject/Applications/Math/Modules/shared.py
@@ -3,34 +3,14 @@
 __author__ = 'Xavier ROSSET'
 
 
-class Circle(object):
-
-    def __init__(self, radius):
-        self.radius = radius
-
-    @property
-    def radius(self):
-        return self._diameter/2
-
-    @radius.setter
-    def radius(self, value):
-        self._diameter = value*2
-
-    @perimeter
-    def perimeter(self):
-        return 2*math.pi*self.radius
-
-    @surface
-    def surface(self):
-        return math.pi*self.radius**2
-
-
 class ArithmeticSequence(object):
 
     def __init__(self, firstterm=1, difference=1, terms=10):
+        self._terms = 0
+        self._difference = 0
+        self._firstterm = firstterm
         self.difference = difference
         self.terms = terms
-        self._firstterm = firstterm
 
     @property
     def sequence(self):
@@ -41,33 +21,9 @@ class ArithmeticSequence(object):
     def series(self):
         return ((self._firstterm + self.lastterm)/2)*self.terms
 
-    # @property
-    # def firstterm(self):
-    #     return self._firstterm
-    #
-    # @firstterm.setter
-    # def firstterm(self, value):
-    #     self._firstterm = value
-    #
-    # @property
-    # def terms(self):
-    #     return self._terms
-    #
-    # @terms.setter
-    # def terms(self, value):
-    #     if value > 9999:
-    #         raise ValueError("Terms above 49999 are not allowed due to system limitations.")
-    #     self._terms = value
-    #
-    # @property
-    # def difference(self):
-    #     return self._difference
-    #
-    # @difference.setter
-    # def difference(self, value):
-    #     if not value:
-    #         raise ValueError("Difference must be greater than 0.")
-    #     self._difference = value
+    @property
+    def terms(self):
+        return self._terms
 
     @terms.setter
     def terms(self, value):
@@ -93,13 +49,15 @@ class ArithmeticSequence(object):
 class GeometricSequence(object):
 
     def __init__(self, firstterm=1, ratio=2, terms=10):
+        self._terms = 0
+        self._ratio = 0
+        self._firstterm = firstterm
         self.terms = terms
         self.ratio = ratio
-        self._firstterm = firstterm
 
     @property
     def sequence(self):
-        return (term for term in self._firstterm*pow(self.ratio, i) for i in range(self.terms)):
+        return (term for term in self._firstterm*pow(self.ratio, i) for i in range(self.terms))
 
     @property
     def series(self):
