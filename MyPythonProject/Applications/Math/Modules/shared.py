05cb92d1bdffcc9f5ad91e7554aab7c1e3388aac
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-08-05 09:26:25 +0200

    Update shared.py

--- MyPythonProject/Applications/Math/Modules/shared.py
+++ MyPythonProject/Applications/Math/Modules/shared.py
@@ -1,71 +1,45 @@
 # -*- coding: ISO-8859-1 -*-
-from math import pow
 
 __author__ = 'Xavier ROSSET'
 
 
-class Sequence(object):
-
-    def __init__(self, firstterm, terms):
-        self.firstterm = firstterm
-        self._terms = terms
-
-
-class ArithmeticSequence(Sequence):
+class ArithmeticSequence(object):
 
     def __init__(self, firstterm=1, difference=1, terms=10):
-        super(ArithmeticSequence, self).__init__(firstterm, terms)
-        self.difference = difference
-
-    def sequence(self):
-        for i in range(self.firstterm, self.firstterm + (self.difference*self.terms), self.difference):
-            yield i
-
-    def series(self):
-        for i in self.sequence(self.firstterm, self.difference, self.terms):
-            series = ((self.firstterm + i)/2)*self.terms
-        return series
+        if terms > 49999:
+            raise ValueError("Terms above 49999 are not allowed due to system limitations.")
+        if difference == 0:
+            raise ValueError("Difference must be greater than 0.")
+        self._difference = difference
+        self._firstterm = firstterm
+        self._terms = terms
 
     @property
-    def terms(self):
-        return self._terms
+    def sequence(self):
+        return (i for i in range(self._firstterm, self._firstterm + (self._difference*self._terms), self._difference))
 
-    @terms.setter
-    def terms(self, value):
-        if value > 9999:
-            raise ValueError("Terms above 9999 are not possible due to system limitation.")
-        self._terms = value
+    @property
+    def series(self):
+        return ((self._firstterm + self.sequence[len(self.sequence) - 1])/2)*self._terms
 
 
-class GeometricSequence(Sequence):
+class GeometricSequence(object):
 
     def __init__(self, firstterm=1, ratio=2, terms=10):
-        super(GeometricSequence, self).__init__(firstterm, terms)
+        if terms > 1499:
+            raise ValueError("Terms above 1499 are not allowed due to system limitations.")
+        if ratio == 0:
+            raise ValueError("Ratio must be greater than 0.")
+        if ratio > 100:
+            raise ValueError("Ratio above 100 is not allowed due to system limitations.")
+        self._difference = difference
+        self._firstterm = firstterm
         self._ratio = ratio
 
+    @property
     def sequence(self):
-        for term in [self.firstterm*pow(self.ratio, i) for i in range(self.terms)]:
-            yield term
+        return (term for term in self._firstterm*pow(self._ratio, i) for i in range(self._terms)):
 
+    @property
     def series(self):
-        return self.firstterm*((pow(self.ratio, self.terms) - 1)/(self.ratio - 1))
-
-    @property
-    def terms(self):
-        return self._terms
-
-    @terms.setter
-    def terms(self, value):
-        if value > 1499:
-            raise ValueError("Terms above 1499 are not possible due to system limitation.")
-        self._terms = value
-
-    @property
-    def ratio(self):
-        return self.ratio
-
-    @ratio.setter
-    def ratio(self, value):
-        if value > 100:
-            raise ValueError("Ratio above 100 is not possible due to system limitation.")
-        self.ratio = value
+        return self._firstterm*((pow(self._ratio, self._terms) - 1)/(self._ratio - 1))
