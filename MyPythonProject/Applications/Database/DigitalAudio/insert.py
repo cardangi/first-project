0f746f3fa62e47ef4a13f2fad32c80c9185056c5
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-08-30 09:06:08 +0200

    Save changes

--- MyPythonProject/Applications/Database/DigitalAudio/insert.py
+++ MyPythonProject/Applications/Database/DigitalAudio/insert.py
@@ -1,11 +1,11 @@
 # -*- coding: ISO-8859-1 -*-
-from contextlib import contextmanager
 from datetime import datetime
 import argparse
 import sqlite3
 import locale
 import csv
-from ... import shared
+from ... import shared as s1
+from ..Modules import shared as s2
 
 __author__ = 'Xavier ROSSET'
 
@@ -20,43 +20,7 @@ locale.setlocale(locale.LC_ALL, "")
 # Arguments parser.
 # =================
 parser = argparse.ArgumentParser()
-parser.add_argument("fromfile", type=argparse.FileType(encoding=shared.DFTENCODING))
-
-
-# ========
-# Classes.
-# ========
-class Boolean(object):
-
-    def __init__(self, s):
-        self.bool = False
-        if s.upper() == "Y":
-            self.bool = True
-
-    def __repr__(self):
-        return "{0}".format(self.bool).lower()
-
-
-# ==========
-# Functions.
-# ==========
-@contextmanager
-def connectto(db):
-    conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
-    conn.row_factory = sqlite3.Row
-    yield conn.cursor()
-    conn.commit()
-    conn.close()
-
-
-def adapt_boolean(b):
-    d = {False: 0, True: 1}
-    return d[b.bool]
-
-
-def convert_boolean(i):
-    d = {0: False, 1: True}
-    return d[int(i)]
+parser.add_argument("fromfile", type=argparse.FileType(encoding=s1.DFTENCODING))
 
 
 def convertyeartointeger(s):
@@ -79,20 +43,20 @@ HEADERS = ["index", "albumsort", "titlesort", "artist", "year", "album", "genre"
 # Initializations.
 # ================
 arguments = parser.parse_args()
-sqlite3.register_adapter(Boolean, adapt_boolean)
-sqlite3.register_converter("boolean", convert_boolean)
+sqlite3.register_adapter(s2.Boolean, s2.adapt_boolean)
+sqlite3.register_converter("boolean", s2.convert_boolean)
 
 
 # ===============
 # Main algorithm.
 # ===============
-with connectto(shared.DATABASE) as c:
+with s2.connectto(s1.DATABASE) as c:
 
     reader = csv.DictReader(arguments.fromfile, delimiter=";", fieldnames=HEADERS)
     for row in reader:
 
         # ALBUMS table.
-        tupalbum = (row["index"][:-11], row["artist"], int(row["year"]), row["album"], int(row["totaldiscs"]), row["genre"], Boolean(row["live"]), Boolean(row["bootleg"]), Boolean(row["incollection"]),
+        tupalbum = (row["index"][:-11], row["artist"], int(row["year"]), row["album"], int(row["totaldiscs"]), row["genre"], s2.Boolean(row["live"]), s2.Boolean(row["bootleg"]), s2.Boolean(row["incollection"]),
                     row["language"], row["upc"], convertyeartointeger(row["encodingyear"]), datetime.now(), convertyeartointeger(row["origyear"]))
         try:
             c.execute("INSERT INTO albums (albumid, artist, year, album, discs, genre, live, bootleg, incollection, language, upc, encodingyear, created, origyear) "
---
+++ MyPythonProject/Applications/Database/Modules/shared.py
@@ -0,0 +1,41 @@
+# -*- coding: ISO-8859-1 -*-
+from contextlib import contextmanager
+import sqlite3
+
+__author__ = 'Xavier ROSSET'
+
+
+# ========
+# Classes.
+# ========
+class Boolean(object):
+
+    def __init__(self, s):
+        self.bool = False
+        if s.upper() == "Y":
+            self.bool = True
+
+    def __repr__(self):
+        return "{0}".format(self.bool).lower()
+
+
+# ==========
+# Functions.
+# ==========
+@contextmanager
+def connectto(db):
+    conn = sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES)
+    conn.row_factory = sqlite3.Row
+    yield conn.cursor()
+    conn.commit()
+    conn.close()
+
+
+def adapt_boolean(b):
+    d = {False: 0, True: 1}
+    return d[b.bool]
+
+
+def convert_boolean(i):
+    d = {0: False, 1: True}
+    return d[int(i)]
