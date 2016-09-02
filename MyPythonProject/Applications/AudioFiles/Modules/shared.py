6227804f1883aa5920f57b5e4f541310553c5f15
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-09-02 21:01:39 +0200

    Optimize formatindexes function

--- MyPythonProject/Applications/AudioFiles/Modules/shared.py
+++ MyPythonProject/Applications/AudioFiles/Modules/shared.py
@@ -1,8 +1,26 @@
 # -*- coding: ISO-8859-1 -*-
 from collections import namedtuple
+from functools import wraps
+import itertools
 import re
 
 __author__ = 'Xavier ROSSET'
+
+
+class Decorator(object):
+
+    def __init__(self, arg):
+        start, end = list(map(int, arg.split("-")))
+        self._start = start
+        self._end = end + 1
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapper():
+            return func(self._start, self._end)
+
+        return wrapper
 
 
 def grabdiscnumber(fil, rex):
@@ -26,18 +44,27 @@ def getfilefromindex(indexes, files):
     return [files[int(i) - 1] for i in indexes_list if int(i) <= len(files)]
 
 
+def myfunction(start, end):
+    """Docstring"""
+    for i in range(start, end):
+        yield i
+
+
 def formatindexes(indexes):
-    out = []
+
+    # Constants.
+    SEP = ", "
+
+    # Regular expressions.
     rex1 = re.compile(r"^\d\d?$")
-    rex2 = re.compile(r"^(\d{1,2})\-(\d{1,2})$")
-    for index in indexes.split(", "):
-        match1 = rex1.match(index)
-        match2 = rex2.match(index)
-        if any([match1, match2]):
-            if match1:
-                out.append(int(index))
-            elif match2:
-                out += list(range(int(match2.group(1)), int(match2.group(2)) + 1))
-    return ", ".join([str(i) for i in sorted(list(set(out)))])
+    rex2 = re.compile(r"^\d\d?\-\d\d?$")
+
+    # Algorithm.
+    if any([rex1.match(index) or rex2.match(index) for index in indexes.split(SEP)]):
+        out1 = [index for index in indexes.split(SEP) if rex1.match(index)]
+        out2 = list(itertools.chain.from_iterable([map(str, i) for i in [list(i) for i in [i(myfunction)() for i in map(Decorator, [index for index in indexes.split(SEP) if rex2.match(index)])]]]))
+        return sorted(set(out1 + out2), key=int)
+    return []
+
 
 
