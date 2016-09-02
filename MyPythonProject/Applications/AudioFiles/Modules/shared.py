2b958f4f755b49f0589250d9606630e40ea9a326
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-09-02 21:23:27 +0200

    Cleaning script

--- MyPythonProject/Applications/AudioFiles/Modules/shared.py
+++ MyPythonProject/Applications/AudioFiles/Modules/shared.py
@@ -65,9 +65,3 @@ def formatindexes(indexes):
         out2 = list(itertools.chain.from_iterable([map(str, i) for i in [list(i) for i in [i(myfunction)() for i in map(Decorator, [index for index in indexes.split(SEP) if rex2.match(index)])]]]))
         return sorted(set(out1 + out2), key=int)
     return []
-
-
-
-
-
-
