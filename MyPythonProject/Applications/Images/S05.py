33bae73d66522ebd6d423eab7df7f2168f889e6c
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-06 13:59:15 +0200

    Update S05.py

--- MyPythonProject/Applications/Images/S05.py
+++ MyPythonProject/Applications/Images/S05.py
@@ -15,19 +15,17 @@ def toto(s):
     match = regex.match(s)
     if match:
         a, b, c = match.groups()
-        if c is not None:
-            c = str(int(c) + 1)
         if c is None:
             c = 0
-        return tuple(map(int, (a, b, c)))
+        elif c is not None:
+            c = str(int(c) + 1)
+        x, y, z = tuple(map(int, (a, b, c)))
+        return s, x, y, z
 
 
 if __name__ == "__main__":
 
-    print(sorted(sorted(sorted(map(toto, ["20160527_095934.jpg", r"20160304_101202(0).jpg", r"20160304_101202.jpg"]), key=itemgetter(2)), key=itemgetter(1)), key=itemgetter(0)))
     tzinfos = {"CET": tz.gettz("Europe/Paris"), "CEST": tz.gettz("Europe/Paris")}
-    # print([timezone("Europe/Paris").localize(y).strftime("%d/%m/%Y %H:%M:%S %Z%z") for
-    #        y in [parser.parse("{0}{1}".format(itemgetter(0)(i), itemgetter(1)(i)), tzinfos=tzinfos)
-    #              for i in sorted(sorted(sorted(map(toto, ["20160527_095934.jpg", r"20160304_101202(0).jpg", r"20160304_101202.jpg"]), key=itemgetter(2)), key=itemgetter(1)), key=itemgetter(0))]])
-    # print(toto(r"20160304_101202(1).jpg"))
-    # print(toto(r"20160307_100504.jpg"))
+    print([(a, timezone("Europe/Paris").localize(b).timestamp()*1000 + c) for
+           a, b, c in [(itemgetter(0)(i), parser.parse("{0}{1}".format(itemgetter(1)(i), itemgetter(2)(i)), tzinfos=tzinfos), itemgetter(3)(i))
+                 for i in sorted(sorted(sorted(map(toto, ["20160527_095934.jpg", r"20160304_101202(0).jpg", r"20160304_101202.jpg"]), key=itemgetter(3)), key=itemgetter(2)), key=itemgetter(1))]])
