a1dd88a535e4dc8d270eb88687d64ae5f4f5e6bc
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-06 14:54:16 +0200

    Update S05.py

--- MyPythonProject/Applications/Images/S05.py
+++ MyPythonProject/Applications/Images/S05.py
@@ -8,30 +8,62 @@ from Applications import shared
 __author__ = 'Xavier ROSSET'
 
 
-pattern = r"^((?:{0})(?:{1})(?:{2}))\B_\B(\d{{6}})(?:\((\d)\))?\.jpg$".format(shared.DFTYEARREGEX, shared.DFTMONTHREGEX, shared.DFTDAYREGEX)
+pattern = r"^((?:{0})(?:{1}))(?:{2})\B_\B\d{{6}}(?:\(\d\))?\.jpg$".format(shared.DFTYEARREGEX, shared.DFTMONTHREGEX, shared.DFTDAYREGEX)
 
 
-class Match(object):
+class OriginalMonth(object):
 
     def __init__(self, pattern):
-        self._pattern = pattern
+        self._regex = None
+        self.regex = pattern
+
+    @property
+    def regex(self):
+        return self._regex
+
+    @regex.setter
+    def regex(self, arg):
+        self._regex = re.compile(arg, re.IGNORECASE)
 
     def __call__(self, s):
-        match = re.match(self._pattern, s)
-        if match:
-            a, b, c = match.groups()
-            if c is None:
-                c = 0
-            elif c is not None:
+        match = self.regex.match(s)
+        if match
+            return s, match.group(1)
+        return None, None
+
+
+class ImageData(object):
+
+    def __init__(self, pattern):
+        self._regex = None
+        self.regex = pattern
+
+    @property
+    def regex(self):
+        return self._regex
+
+    @regex.setter
+    def regex(self, arg):
+        self._regex = re.compile(arg, re.IGNORECASE)
+
+    def __call__(self, s):
+        match = self.regex.match(s)
+        if match
+            a, b, c = re.split(r"\D", s)[:3]
+            if not c:
+                c = "0"
+            else:
                 c = str(int(c) + 1)
-            x, y, z = tuple(map(int, (a, b, c)))
-            return s, x, y, z
-        return None, None, None, None
+            a, b, c = tuple(map(int, (a, b, c)))
+            return timezone("Europe/Paris").localize(parser.parse("{0}{1}".format(a, b))).timestamp()*1000 + c
+        return None
 
 
 if __name__ == "__main__":
 
-    tzinfos = {"CET": tz.gettz("Europe/Paris"), "CEST": tz.gettz("Europe/Paris")}
-    print([(a, timezone("Europe/Paris").localize(b).timestamp()*1000 + c) for
-           a, b, c in [(itemgetter(0)(i), parser.parse("{0}{1}".format(itemgetter(1)(i), itemgetter(2)(i)), tzinfos=tzinfos), itemgetter(3)(i))
-                 for i in sorted(sorted(sorted(map(Toto(pattern), ["20160527_095934.jpg", r"20160304_101202(0).jpg", r"20160304_101202.jpg"]), key=itemgetter(3)), key=itemgetter(2)), key=itemgetter(1)) if itemgetter(0)]])
+	reflist = [(fil, month) in map(OriginalMonth(pattern), list(shared.filesinfolder(["jpg"], folder=src))) if fil]
+
+    # tzinfos = {"CET": tz.gettz("Europe/Paris"), "CEST": tz.gettz("Europe/Paris")}
+    # print([(a, timezone("Europe/Paris").localize(b).timestamp()*1000 + c) for
+           # a, b, c in [(itemgetter(0)(i), parser.parse("{0}{1}".format(itemgetter(1)(i), itemgetter(2)(i)), tzinfos=tzinfos), itemgetter(3)(i))
+                 # for i in sorted(sorted(sorted(map(GetImageData(pattern), ["20160527_095934.jpg", r"20160304_101202(0).jpg", r"20160304_101202.jpg"]), key=itemgetter(3)), key=itemgetter(2)), key=itemgetter(1)) if itemgetter(0)]])
