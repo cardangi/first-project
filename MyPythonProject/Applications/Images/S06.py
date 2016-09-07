6653246251470cb7492f03dbbe4970d4da5cdd10
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-07 17:33:18 +0200

    Update S06.py

--- MyPythonProject/Applications/Images/S06.py
+++ MyPythonProject/Applications/Images/S06.py
@@ -11,7 +11,7 @@ for key in sorted(list(c)):
     l1 = [(itemgetter(0)(i), itemgetter(1)(i)) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) == key]  # [("file1", 1234567890), ("file2", 1234567890)]
     l2 = list(zip(*l1))  # [("file1", "file2"), (1234567890, 1234567890)]
     l3 = list(map(toto, l2[1], range(len(l2[1]))))  # [1234567890000, 1234567890001]
-    z2 = list(zip(l2[0], l3))  # [("file1", 1234567890000), ("file2", 1234567890001)]
+    z2 = list(zip(l2[0], l3))  # [("file1", 1234567890000), ("file2", 1234567890001), ("file3", 1234567890003)]
     z1 += z2
 
 x = sorted(z1, key=itemgetter(1))
