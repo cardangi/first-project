61105d297a488f515edb29a410c2e06b7f4650c5
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-07 16:27:28 +0200

    Update S05.py

--- MyPythonProject/Applications/Images/S05.py
+++ MyPythonProject/Applications/Images/S05.py
@@ -61,6 +61,17 @@ def validpath(p):
     return p
 
 
+def decorator(func):
+    def wrapper(char="="):
+        return "{0} {1} {0}".format(char*50, func())
+    return wrapper
+
+
+@decorator
+def now():
+    return shared.dateformat(datetime.now(tz=timezone(shared.DFTTIMEZONE)), shared.TEMPLATE1)
+
+
 # =================
 # Arguments parser.
 # =================
@@ -85,7 +96,8 @@ included, excluded, arguments = list(), list(), parser.parse_args()
 # ==============
 # Log arguments.
 # ==============
-logger.info("{0} {1} {0}".format("="*50, shared.dateformat(datetime.now(tz=timezone(shared.DFTTIMEZONE)), shared.TEMPLATE1)))
+# logger.info(now())
+logger.info("{0:=^140s}".format(" {0} ".format(shared.dateformat(datetime.now(tz=timezone(shared.DFTTIMEZONE)), shared.TEMPLATE1))))
 logger.info('START "%s".' % (os.path.basename(__file__),))
 logger.debug("Source directory.")
 logger.debug('\t"{0}".'.format(arguments.src).expandtabs(3))
