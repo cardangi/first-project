7dbd17cd7d29e138f9306fd6c5523a748f160b58
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-09-07 19:46:49 +0200

    Use "itertools.count" to work with duplicates

--- MyPythonProject/Applications/Images/S06.py
+++ MyPythonProject/Applications/Images/S06.py
@@ -6,20 +6,15 @@ from itertools import count
 __author__ = 'Xavier ROSSET'
 
 
-def toto(x, i):
-    return x*1000 + i
-
-
-x = [("file1", 1234567890), ("file2", 1234567890), ("file3", 1234567891)]
+x = [("file1", 1234567890), ("file2", 1234567890), ("file3", 1234567891), ("file4", 1234567892), ("file5", 1234567893), ("file6", 1234567893)]
 c = Counter([itemgetter(1)(i) for i in x])
 y = [key for key in sorted(list(c)) if c[key] > 1]
 z1 = [(itemgetter(0)(i), itemgetter(1)(i)*1000) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) not in y]
 
 for key in y:
-    l1 = [(itemgetter(0)(i), itemgetter(1)(i)) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) == key]  # [("file1", 1234567890), ("file2", 1234567890)]
-    l2 = list(zip(*l1))  # [("file1", "file2"), (1234567890, 1234567890)]
-    l3 = list(map(toto, l2[1], range(len(l2[1]))))  # [1234567890000, 1234567890001]
-    z2 = list(zip(l2[0], l3))  # [("file1", 1234567890000), ("file2", 1234567890001), ("file3", 1234567890002)]
-    z1 += z2
+    l1 = [itemgetter(0)(i) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) == key]  # ["file1", "file2", "file3"]
+    l2 = list(zip(l1, count(key*1000)))  # [("file1", 1234567890000), ("file2", 1234567890001), ("file3", 1234567890002)]
+    z1 += l2
 
-x = sorted(z1, key=itemgetter(1))
+print(sorted(z1, key=itemgetter(1)))
+
