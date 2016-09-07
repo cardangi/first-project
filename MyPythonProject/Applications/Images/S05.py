e06fb4c5fcf4da6197c02a6dd773b32c00ee7b82
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-07 16:18:13 +0200

    Update S05.py

--- MyPythonProject/Applications/Images/S05.py
+++ MyPythonProject/Applications/Images/S05.py
@@ -1,11 +1,19 @@
 # -*- coding: ISO-8859-1 -*-
 import os
+import logging
+import argparse
 from collections import Counter
 from operator import itemgetter
 from contextlib import contextmanager
 from Applications import shared
 
 __author__ = 'Xavier ROSSET'
+
+
+# ========
+# Logging.
+# ========
+logger = logging.getLogger("%s.%s" % (__package__, os.path.basename(__file__)))
 
 
 # ========
@@ -45,16 +53,42 @@ def chgcurdir(d):
             os.chdir(wcdir)
 
 
+def validpath(p):
+    if not os.path.exists(p):
+        raise argparse.ArgumentTypeError('"{0}" doesn\'t exist'.format(p))
+    if not os.path.isdir(p):
+        raise argparse.ArgumentTypeError('"{0}" is not a directory'.format(p))
+    return p
+
+
+# =================
+# Arguments parser.
+# =================
+parser = argparse.ArgumentParser()
+parser.add_argument("src", type=validpath)
+parser.add_argument("--rename", action="store_true")
+parser.add_argument("--test", action="store_true")
+
+
 # ==========
 # Constants.
 # ==========
-SRC = r"G:\Videos\Samsung S5"
+# arguments.src = r"G:\Videos\Samsung S5"
 
 
 # ================
 # Initializations.
 # ================
-included, excluded = list(), list()
+included, excluded, arguments = list(), list(), parser.parse_args()
+
+
+# ==============
+# Log arguments.
+# ==============
+logger.info("{0} {1} {0}".format("="*50, shared.dateformat(datetime.now(tz=timezone(shared.DFTTIMEZONE)), shared.TEMPLATE1)))
+logger.info('START "%s".' % (os.path.basename(__file__),))
+logger.debug("Source directory.")
+logger.debug('\t"{0}".'.format(arguments.src).expandtabs(3))
 
 
 # ===============
@@ -62,8 +96,10 @@ included, excluded = list(), list()
 # ===============
 
 
+#     ----------
 #  1. Get files.
-for fil in shared.imagesinfolder(["jpg"], folder=SRC):
+#     ----------
+for fil in shared.imagesinfolder(["jpg"], folder=arguments.src):
     try:
         obj = shared.SamsungS5(fil)
     except shared.ExifError:
@@ -72,27 +108,61 @@ for fil in shared.imagesinfolder(["jpg"], folder=SRC):
         included.append((fil, obj))
 
 
+#     ---------------
 #  2. Group by month.
+#     ---------------
 c = Counter(["{0}{1}".format(itemgetter(1)(i).originalyear, itemgetter(1)(i).originalmonth) for i in included if itemgetter(1)(i).match])
 
 
-#  3. Copy and rename.
+#     ---------------
+#  3. Log statistics.
+#     ---------------
+logger.debug("{0:>4d} file(s) found.".format(sum(c.values())))
+for month in sorted(list(c), key=int):
+    logger.debug("\t{0}: {1:>4d} file(s).".format(month, c[month]))
+
+
+#     ----------------
+#  4. Copy and rename.
+#     ----------------
 for month in sorted(list(c), key=int):
     included, curdir = list(), os.path.join(r"H:\\", month)
     if not os.path.exists(curdir):
 
         # Copy.
-        copytree(SRC, curdir, ignore=IgnoreBut(r"^({0})({1})\B_\B(\d{{6}})(?:\((\d)\))?\.jpg$".format(curdir, DFTDAYREGEX)))
+        logger.debug('Copy files using "shutil.copytree".')
+        logger.debug('\tSource\t\t: "{0}".'.format(arguments.src).expandtabs(3))
+        logger.debug('\tDestination : "{0}".'.format(curdir).expandtabs(3))
+        logger.debug('\tFiles\t\t\t : {0:>4d} file(s) to copy.'.format(c[curdir]).expandtabs(3))
+        copytree(arguments.src, curdir, ignore=IgnoreBut(r"^({0})({1})\B_\B(\d{{6}})(?:\((\d)\))?\.jpg$".format(curdir, DFTDAYREGEX)))
 
         # Rename.
-        with chgcurdir(curdir) as exception:
-            if not exception:
-                for fil in shared.imagesinfolder(["jpg"]):
-                    try:
-                        obj = shared.SamsungS5(fil)
-                    except shared.ExifError:
-                        pass
-                    else:
-                        included.append((fil, obj))
-                for src, dst in included:
-                    os.rename(src=src, dst="{0}.jpg".format(dst))
+        if arguments.rename:
+            with chgcurdir(curdir) as exception:
+                logger.debug("Change current working directory.")
+                logger.debug('\t"{0}" set as current working directory.'.format(curdir).expandtabs(3))
+                if not exception:
+                    for fil in shared.imagesinfolder(["jpg"]):
+                        try:
+                            obj = shared.SamsungS5(fil)
+                        except shared.ExifError:
+                            pass
+                        else:
+                            included.append((fil, obj))
+                    for arguments.src, dst in included:
+                        logger.debug("Rename file.")
+                        logger.debug('\tBefore: "{0}".'.format(arguments.src).expandtabs(3))
+                        logger.debug('\tAfter : "{0}.jpg".'.format(dst).expandtabs(3))
+                        if not arguments.test:
+                            try:
+                                os.rename(arguments.src=arguments.src, dst="{0}.jpg".format(dst))
+                            except OSError:
+                                logger.debug("\tRename failed.".expandtabs(4))
+                            else:
+                                logger.debug("\tRename succeeded.".expandtabs(4))
+
+
+# ===============
+# Exit algorithm.
+# ===============
+logger.info('END "%s".' % (os.path.basename(__file__),))
