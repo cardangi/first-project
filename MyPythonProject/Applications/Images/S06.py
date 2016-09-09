91431d4504d84b1c09da78aadb24d256f1bd962f
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-09-09 15:23:22 +0200

    Update S06.py

--- MyPythonProject/Applications/Images/S06.py
+++ MyPythonProject/Applications/Images/S06.py
@@ -1,31 +1,4 @@
 # -*- coding: ISO-8859-1 -*-
-from operator import itemgetter
-from collections import Counter
-from itertools import count
-from Applications import shared
-
-__author__ = 'Xavier ROSSET'
-
-
-x = [("file1", 1234567890), ("file2", 1234567890), ("file3", 1234567891), ("file4", 1234567892), ("file5", 1234567893), ("file6", 1234567893), ("file7", 1234567890)]
-c = Counter([itemgetter(1)(i) for i in x])
-y = [key for key in sorted(list(c)) if c[key] > 1]
-z1 = [(itemgetter(0)(i), itemgetter(1)(i)*1000) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) not in y]
-
-for key in y:
-    l1 = [itemgetter(0)(i) for i in sorted(sorted(x, key=itemgetter(0)), key=itemgetter(1)) if itemgetter(1)(i) == key]  # ["file1", "file2", "file3"]
-    l2 = list(zip(l1, count(key*1000)))  # [("file1", 1234567890000), ("file2", 1234567890001), ("file3", 1234567890002)]
-    z1 += l2
-
-print(sorted(z1, key=itemgetter(1)))
-
-
-obj = shared.Images(r"H:\201005\201005_00400.jpg")
-print(sorted(obj.exif.keys()))
-print(obj.exif[37500])
-print(obj.exif[37520])
-print(obj.exif[37521])
-
 from collections import Counter
 from operator import itemgetter
 from itertools import count
@@ -82,6 +55,3 @@ class ImagesCollection(object):
 x = ImagesCollection([("file1", 1234567890000, "201605"), ("file2", 1234567890000, "201605"), ("file3", 1234567890000, "201605"), ("file4", 1234567891000, "201605")])
 for i in x:
     os.rename(src=itemgetter(0)(i), dst=itemgetter(1)(i))
-
-print(obj.exif[37522])
-
