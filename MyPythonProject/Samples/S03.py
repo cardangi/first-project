7db351db99104ad768ef42771054cb77c24fc353
Xavier <cestmoibalibalo@users.noreply.github.com>
2016-09-10 13:31:35 +0200

    .

--- MyPythonProject/Samples/S03.py
+++ MyPythonProject/Samples/S03.py
@@ -1,5 +1,4 @@
 # -*- coding: ISO-8859-1 -*-
-
 __author__ = 'Xavier ROSSET'
 
 
@@ -12,7 +11,6 @@ def myfunc2(s):
 
 
 x = ["2016_00001", "2016_00002", "2016_00003", "2016_00101", "2015_00456"]
-
 print(int(max(x).split("_")[1]))
 print(int(max([i.split("_")[1] for i in x])))
 print(sorted(x, key=myfunc1))
