515e557a84b3ff2fdca5f3e0854587fc4c894290
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-10-06 21:55:06 +0200

    Save changes

--- MyPythonProject/Applications/AudioFiles/importFiles.py
+++ MyPythonProject/Applications/AudioFiles/importFiles.py
@@ -1,5 +1,5 @@
 # -*- coding: ISO-8859-1 -*-
-from collections import MutableMapping, MutableSequence, Counter, deque, namedtuple
+from collections import MutableMapping, MutableSequence, deque, namedtuple
 from jinja2 import Environment, FileSystemLoader
 from mutagen import File, MutagenError
 from itertools import groupby, repeat
@@ -10,6 +10,7 @@ from functools import wraps
 from pytz import timezone
 from shutil import copy2
 import subprocess
+import argparse
 import logging
 import locale
 import sys
@@ -241,6 +242,13 @@ def groupbycodec(t):
     return t.codec
 
 
+# ==========
+# Arguments.
+# ==========
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--test", action="store_true")
+
+
 # ========
 # Logging.
 # ========
@@ -299,13 +307,14 @@ CURWDIR, TABSIZE1, TABSIZE2 = os.path.join(os.path.expandvars("%_MYMUSIC%"), r"B
 # ==================
 # Initializations 1.
 # ==================
-header, artists, albums, codecs, index, code, status, curwdir, tracks, files, args, statuss, tmpl, choice = None, None, None, None, 0, 1, 100, CURWDIR, [], {}, [], [], "", ""
+header, artists, albums, codecs, index, code, status, curwdir, tracks, files, args, fails, successes, statuss, tmpl, choice, arguments = None, None, None, None, 0, 1, 100, CURWDIR, [], {}, [], [], [], [], "", "", \
+                                                                                                                                         parser.parse_args()
 
 
 # ==================
 # Initializations 2.
 # ==================
-justify, nt = "".join(list(repeat("\n", 3))), namedtuple("nt", "maintitle step title")
+justify, nt1, nt2 = "".join(list(repeat("\n", 3))), namedtuple("nt1", "maintitle step title"), namedtuple("nt2", "file dirname basename extension")
 
 
 # ===============
@@ -317,39 +326,40 @@ while True:
     #  1. Grab available artists from current directory.
     #     ----------------------------------------------
     if code == 1:
-        header = shared.Header("import  audio  files", ["Set current directory.", "Set artist.", "Set album", "Set codec", "Import files.", "Run import.", "Exit program."])
+        header = shared.Header("import  audio  files", ["Set current directory.", "Set artist.", "Set album", "Set codec", "Import files.", "Run import.", "Tag files", "Exit program."])
         head = header()
-        tmpl = template1.render(header=nt(*head), message=['Current directory is: "{0}"'.format(CURWDIR)])
-        tracks.clear()
+        tmpl = template1.render(header=nt1(*head), message=['Current directory is: "{0}"'.format(CURWDIR)])
+        successes.clear()
         statuss.clear()
+        fails.clear()
         while True:
             print(clearscreen(t=tmpl))
             choice = input("{0}\tWould you like to change the current directory [Y/N]? ".format(justify).expandtabs(TABSIZE1))
             if choice.upper() in shared.ACCEPTEDANSWERS:
                 break
         if choice.upper() == "Y":
-            tmpl = template1.render(header=nt(*head))
+            tmpl = template1.render(header=nt1(*head))
             while True:
                 print(clearscreen(t=tmpl))
                 curwdir = input("{0}\tPlease enter directory: ".format(justify).expandtabs(TABSIZE1))
                 if curwdir:
                     if not os.path.exists(curwdir):
-                        tmpl = template1.render(header=nt(*head), message=['"{0}" is not a valid directory!'.format(curwdir)])
+                        tmpl = template1.render(header=nt1(*head), message=['"{0}" is not a valid directory!'.format(curwdir)])
                         continue
                     if not os.path.isdir(curwdir):
-                        tmpl = template1.render(header=nt(*head), message=['"{0}" is not a valid directory!'.format(curwdir)])
+                        tmpl = template1.render(header=nt1(*head), message=['"{0}" is not a valid directory!'.format(curwdir)])
                         continue
                     break
-                tmpl = template1.render(header=nt(*head))
+                tmpl = template1.render(header=nt1(*head))
         artists = [(k, list(v)) for k, v in groupby(AudioFiles.fromfolder(folder=curwdir), key=groupbyartist)]
         kartists = [itemgetter(0)(artist) for artist in artists]
         head = header()
         code = 2
-        tmpl = template1.render(header=nt(*head), list1=kartists)
+        tmpl = template1.render(header=nt1(*head), list1=kartists)
         if not kartists:
             head = shared.Header("import  audio  files", ["Exit program."], 2)()
             code = 99
-            tmpl = template1.render(header=nt(*head), message=["No artists found."])
+            tmpl = template1.render(header=nt1(*head), message=["No artists found."])
         logger.debug("--------------")
         logger.debug("Found artists.")
         logger.debug("--------------")
@@ -367,23 +377,23 @@ while True:
                 try:
                     index = int(choice)
                 except ValueError:
-                    tmpl = template1.render(header=nt(*head), list1=kartists, message=['"{0}" is not a valid input'.format(choice)])
+                    tmpl = template1.render(header=nt1(*head), list1=kartists, message=['"{0}" is not a valid input'.format(choice)])
                     continue
                 else:
                     if index > len(kartists):
-                        tmpl = template1.render(header=nt(*head), list1=kartists, message=['"{0}" is not a valid input'.format(choice)])
+                        tmpl = template1.render(header=nt1(*head), list1=kartists, message=['"{0}" is not a valid input'.format(choice)])
                         continue
                     break
-            tmpl = template1.render(header=nt(*head), list1=kartists)
+            tmpl = template1.render(header=nt1(*head), list1=kartists)
         albums = [(k, list(v)) for k, v in groupby(artists[index - 1][1], key=groupbyalbum)]
         kalbums = [itemgetter(0)(album) for album in albums]
         head = header()
         code = 3
-        tmpl = template1.render(header=nt(*head), list1=kalbums)
+        tmpl = template1.render(header=nt1(*head), list1=kalbums)
         if not kalbums:
             head = shared.Header("import  audio  files", ["Exit program."], 3)()
             code = 99
-            tmpl = template1.render(header=nt(*head), message=['No albums found.'])
+            tmpl = template1.render(header=nt1(*head), message=['No albums found.'])
         logger.debug("-------------")
         logger.debug("Found albums.")
         logger.debug("-------------")
@@ -401,23 +411,23 @@ while True:
                 try:
                     index = int(choice)
                 except ValueError:
-                    tmpl = template1.render(header=nt(*head), list1=kalbums, message=['"{0}" is not a valid input'.format(choice)])
+                    tmpl = template1.render(header=nt1(*head), list1=kalbums, message=['"{0}" is not a valid input'.format(choice)])
                     continue
                 else:
                     if index > len(kalbums):
-                        tmpl = template1.render(header=nt(*head), list1=kalbums, message=['"{0}" is not a valid input'.format(choice)])
+                        tmpl = template1.render(header=nt1(*head), list1=kalbums, message=['"{0}" is not a valid input'.format(choice)])
                         continue
                     break
-            tmpl = template1.render(header=nt(*head), list1=kalbums)
+            tmpl = template1.render(header=nt1(*head), list1=kalbums)
         codecs = [(k, list(v)) for k, v in groupby(albums[index - 1][1], key=groupbycodec)]
         kcodecs = [itemgetter(0)(codec) for codec in codecs]
         head = header()
         code = 4
-        tmpl = template1.render(header=nt(*head), list1=kcodecs)
+        tmpl = template1.render(header=nt1(*head), list1=kcodecs)
         if not kcodecs:
             head = shared.Header("import  audio  files", ["Exit program."], 4)()
             code = 99
-            tmpl = template1.render(header=nt(*head), message=['No codecs found.'])
+            tmpl = template1.render(header=nt1(*head), message=['No codecs found.'])
         logger.debug("-------------")
         logger.debug("Found codecs.")
         logger.debug("-------------")
@@ -435,60 +445,177 @@ while True:
                 try:
                     index = int(choice)
                 except ValueError:
-                    tmpl = template1.render(header=nt(*head), list1=kcodecs, message=['"{0}" is not a valid input'.format(choice)])
+                    tmpl = template1.render(header=nt1(*head), list1=kcodecs, message=['"{0}" is not a valid input'.format(choice)])
                     continue
                 else:
                     if index > len(kcodecs):
-                        tmpl = template1.render(header=nt(*head), list1=kcodecs, message=['"{0}" is not a valid input'.format(choice)])
+                        tmpl = template1.render(header=nt1(*head), list1=kcodecs, message=['"{0}" is not a valid input'.format(choice)])
                         continue
                     break
-            tmpl = template1.render(header=nt(*head), list1=kcodecs)
-        sources = [itemgetter(6)(item) for item in codecs[index - 1][1]]
-        extensions = [os.path.splitext(itemgetter(6)(item))[1] for item in codecs[index - 1][1]]
-        destinations = [(a, b, c, d) for a, (b, c), d in list(zip(sources,
-                                                                  [(template2.substitute(year=track.year,
-                                                                                         month="{0:0>2d}".format(track.month),
-                                                                                         day="{0:0>2d}".format(track.day),
-                                                                                         location=track.location,
-                                                                                         disc=track.discnumber
-                                                                                         ),
-                                                                    "{0}.{1}".format(track.albumsort, track.titlesort)) for track in map(getattributes, [itemgetter(7)(item) for item in codecs[index - 1][1]])
-                                                                   if track],
-                                                                  extensions
-                                                                  )
+            tmpl = template1.render(header=nt1(*head), list1=kcodecs)
+        sources, extensions = [itemgetter(6)(item) for item in codecs[index - 1][1]], [os.path.splitext(itemgetter(6)(item))[1] for item in codecs[index - 1][1]]
+        files = [nt2(a, b, c, d) for a, (b, c), d in list(zip(sources,
+                                                              [(template2.substitute(year=track.year,
+                                                                                     month="{0:0>2d}".format(track.month),
+                                                                                     day="{0:0>2d}".format(track.day),
+                                                                                     location=track.location,
+                                                                                     disc=track.discnumber),
+                                                                "{0}.{1}".format(track.albumsort, track.titlesort)) for track in map(getattributes, [itemgetter(7)(item) for item in codecs[index - 1][1]])
+                                                               if track],
+                                                              extensions
                                                               )
-                        ]
+                                                          )
+                    ]
         logger.debug("------")
         logger.debug("Files.")
         logger.debug("------")
         for num, source in enumerate(sources, 1):
             logger.debug("{0:>3d}. {1}".format(num, source))
-        logger.debug("-------------")
-        logger.debug("Destinations.")
-        logger.debug("-------------")
-        for num, (a, b, c, d) in enumerate(destinations, 1):
-            logger.debug("{0:>3d}. {1}".format(num, a))
-            logger.debug("\t{0}".format(b).expandtabs(5))
-            logger.debug("\t{0}".format(c).expandtabs(5))
-            logger.debug("\t{0}".format(d).expandtabs(5))
+        logger.debug("----------")
+        logger.debug("Arguments.")
+        logger.debug("----------")
+        for num, (path, dirname, basename, extension) in enumerate(files, 1):
+            logger.debug("{0:>3d}. {1}".format(num, path))
+            logger.debug("\t{0}".format(dirname).expandtabs(5))
+            logger.debug("\t{0}".format(basename).expandtabs(5))
+            logger.debug("\t{0}".format(extension).expandtabs(5))
         head = header()
-        code = 99
-        tmpl = template1.render(header=nt(*head),
+        code = 5
+        tmpl = template1.render(header=nt1(*head),
                                 list2=[
                                     (
                                         "Source\t\t: {0}".format(itemgetter(0)(item)).expandtabs(TABSIZE2),
                                         "Destination\t: {0}{1}".format(os.path.join(itemgetter(1)(item), itemgetter(2)(item)), itemgetter(3)(item)).expandtabs(TABSIZE2)
                                     )
-                                    for item in destinations],
-                                trailer=["{0:>3d} files ready to be imported.".format(len(destinations))]
+                                    for item in files],
+                                trailer=["{0:>3d} files ready to be imported.".format(len(files))]
                                 )
         if not sources:
             head = shared.Header("import  audio  files", ["Exit program."], 5)()
             code = 99
-            tmpl = template1.render(header=nt(*head), message=['No files found.'])
+            tmpl = template1.render(header=nt1(*head), message=['No files found.'])
 
     #     -------------
-    #  5. Exit program.
+    #  5. Import files.
+    #     -------------
+    elif code == 5:
+        while True:
+            print(clearscreen(t=tmpl))
+            choice = input("{0}\tWould you like to import files [Y/N]? ".format(justify).expandtabs(TABSIZE1))
+            if choice.upper() in shared.ACCEPTEDANSWERS:
+                break
+        head = header()
+        code = 6
+        if choice.upper() == "Y":
+            args.extend(files)
+        elif choice.upper() == "N" and not args:
+            head = shared.Header("import  audio  files", ["Exit program."], 6)()
+            code = 99
+        tmpl = template1.render(header=nt1(*head))
+
+    #     -----------
+    #  6. Run import.
+    #     -----------
+    elif code == 6:
+        while True:
+            print(clearscreen(t=tmpl))
+            choice = input("{0}\tWould you like to run import [Y/N]? ".format(justify).expandtabs(TABSIZE1))
+            if choice.upper() in shared.ACCEPTEDANSWERS:
+                break
+        head = header()
+        code = 7
+        tmpl = template1.render(header=nt1(*head))
+
+        # --> 1. Don't run import.
+        if choice.upper() == "N":
+            head = shared.Header("import  audio  files", ["Exit program."], 7)()
+            code = 99
+            tmpl = template1.render(header=nt1(*head))
+
+        # --> 2. Run import.
+        elif choice.upper() == "Y":
+            logger.debug("----------")
+            logger.debug("Arguments.")
+            logger.debug("----------")
+            for num, (path, dirname, basename, extension) in enumerate(args, 1):
+                logger.debug("{0:>3d}. {1}".format(num, path))
+                logger.debug("\t{0}".format(dirname).expandtabs(5))
+                logger.debug("\t{0}".format(basename).expandtabs(5))
+                logger.debug("\t{0}".format(extension).expandtabs(5))
+
+            # --> 2.a. Test mode.
+            if arguments.test:
+                logger.debug("----------")
+                logger.debug("Arguments.")
+                logger.debug("----------")
+                for num, (path, dirname, basename, extension) in enumerate(args, 1):
+                    dst = "{0}{1}".format(os.path.join(dirname, basename), extension)
+                    logger.debug("{0:>3d}. Source     : {1}".expandtabs(5).format(num, path))
+                    logger.debug("\tDestination: {0}".expandtabs(5).format(dst))
+                    statuss.append(0)
+
+            # --> 2.b. Copy mode.
+            elif not arguments.test:
+
+                for item in args:
+                    dst = "{0}{1}".format(os.path.join(item.dirname, item.basename), item.extension)
+                    try:
+                        copy2(src=item.file, dst=dst)
+                    except FileExistsError:
+                        statuss.append(100)
+                        fails.append(dst)
+                    else:
+                        statuss.append(0)
+                        successes.append(dst)
+
+                if successes:
+                    logger.debug("----------")
+                    logger.debug("Successes.")
+                    logger.debug("----------")
+                    for num, file in enumerate(successes, 1):
+                        logger.debug(num, file)
+                if fails:
+                    logger.debug("------")
+                    logger.debug("Fails.")
+                    logger.debug("------")
+                    for num, file in enumerate(fails, 1):
+                        logger.debug(num, file)
+
+            # -> 2.c. All commands failed.
+            if all(map(ne, statuss, repeat(0))):
+                head = shared.Header("import  audio  files", ["Exit program."], 7)()
+                code = 99
+                tmpl = template1.render(header=nt1(*head))
+
+            # --> 2.d. At least one command failed.
+            elif any(map(ne, statuss, repeat(0))):
+                tmpl = template1.render(header=nt1(*head))
+
+    #     ----------
+    #  7. Tag files.
+    #     ----------
+    elif code == 7:
+        while True:
+            print(clearscreen(t=tmpl))
+            choice = input("{0}\tWould you like to tag copied files [Y/N]? ".format(justify).expandtabs(TABSIZE1))
+            if choice.upper() in shared.ACCEPTEDANSWERS:
+                break
+        head = shared.Header("import  audio  files", ["Exit program."], 8)()
+        code = 99
+        tmpl = template1.render(header=nt1(*head))
+        if choice.upper() == "Y":
+            # for file in successes:
+            #     try:
+            #         track = ExtendedTrack(file, codecs[index - 1][1])
+            #     except MutagenError:
+            #         pass
+            #     else:
+            #         track.update(**{"artistsort": mapartist(artists[index - 1][1]), "albumartistsort": mapartist(artists[index - 1][1])})
+            code = 99
+            tmpl = template1.render(header=nt1(*head))
+
+    #     -------------
+    #  8. Exit program.
     #     -------------
     elif code == 99:
         while True:
@@ -501,6 +628,12 @@ while True:
         elif choice.upper() == "Y":
             status = 99
             break
+
+
+# ============
+# End logging.
+# ============
+logger.info('END "%s".' % (os.path.basename(__file__),))
 
 
 # =============
