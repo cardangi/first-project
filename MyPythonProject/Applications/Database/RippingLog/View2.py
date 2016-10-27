0fc2bd44f6aed383881bc6c1c9b6835db59771f4
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-10-27 23:09:16 +0200

    Improve CDRipper.AudioCDRipper.py

--- MyPythonProject/Applications/Database/RippingLog/View2.py
+++ MyPythonProject/Applications/Database/RippingLog/View2.py
@@ -21,7 +21,7 @@ with open(OUTPUT, mode=shared.WRITE, encoding="UTF_8") as fp:
     conn = sqlite3.connect(shared.DATABASE, detect_types=sqlite3.PARSE_DECLTYPES)
     conn.row_factory = sqlite3.Row
     args = []
-    for row in conn.cursor().execute("SELECT rowid AS uid, ripped, artistsort, albumsort, artist, year, album, genre, upc, application FROM rippinglog"):
+    for row in conn.cursor().execute("SELECT rowid AS uid, ripped, artistsort, albumsort, artist, year, album, genre, upc, application FROM rippinglog ORDER BY uid DESC"):
         dict1 = dict(zip(KEYS, [shared.dateformat(shared.LOCAL.localize(row["ripped"]), shared.TEMPLATE4),
                                 row["artistsort"],
                                 row["albumsort"],
