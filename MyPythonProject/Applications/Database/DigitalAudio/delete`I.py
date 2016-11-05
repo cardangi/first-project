a11fdfc780f7715555dbc988e9e957ba5571130b
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-11-05 07:00:28 +0100

    New interfaces to delete database records

--- MyPythonProject/Applications/Database/DigitalAudio/delete`I.py
+++ MyPythonProject/Applications/Database/DigitalAudio/delete`I.py
@@ -1,2 +1,40 @@
 # -*- coding: ISO-8859-1 -*-
+import json
+import sys
+import os
+import re
+
 __author__ = 'Xavier ROSSET'
+
+
+# ==========
+# Constants.
+# ==========
+JSON = os.path.join(os.path.expandvars("%TEMP%"), "arguments.json")
+
+
+# ====================
+# Regular expressions.
+# ====================
+regex = re.compile("\d(?:\d(?:\d(?:\d)?)?)?")
+
+
+# ================
+# Initializations.
+# ================
+arguments, status = None, 100
+
+
+# ===============
+# Main algorithm.
+# ===============
+while True:
+    numbers = input("Please enter record(s) unique ID: ")
+    arguments = regex.findall(numbers)
+    if arguments:
+        break
+if arguments:
+    status = 0
+    with open(JSON, mode="w") as fp:
+        json.dump(arguments, fp)
+sys.exit(status)
--- MyPythonProject/Database`RippingLog`delete`L.py
+++ MyPythonProject/Database`RippingLog`delete`L.py
@@ -1,2 +1,58 @@
 # -*- coding: ISO-8859-1 -*-
+from contextlib import contextmanager, ExitStack
+import subprocess
+import json
+import sys
+import os
+
 __author__ = 'Xavier ROSSET'
+
+
+# ==========
+# Constants.
+# ==========
+JSON = os.path.join(os.path.expandvars("%TEMP%"), "arguments.json")
+
+
+# ================
+# Initializations.
+# ================
+numbers, status = None, 100
+
+
+# =========
+# Commands.
+# =========
+cmd1 = ["python", "-m", "Applications.Database.RippingLog.delete`I"]  # "I" means Interface! "L" means Launcher!
+cmd2 = ["python", "-m", "Applications.CDRipper.AudioCDRippingLog", "delete"]
+
+
+# ==========
+# Functions.
+# ==========
+@contextmanager
+def chgcurdir(d):
+    wcdir = os.getcwd()
+    os.chdir(d)
+    yield
+    os.chdir(wcdir)
+
+
+# ===============
+# Main algorithm.
+# ===============
+with chgcurdir(os.path.expandvars("%_PYTHONPROJECT%")):
+    task1 = subprocess.run(cmd1)
+    status = task1.returncode
+    if not task1.returncode:
+        if os.path.exists(JSON):
+            stack = ExitStack()
+            stack.callback(os.remove, JSON)
+            with stack:
+                with open(JSON) as fp:
+                    numbers = json.load(fp)
+    if numbers:
+        cmd2.extend(numbers)
+        task2 = subprocess.run(cmd2)
+        status = task2.returncode
+sys.exit(status)
