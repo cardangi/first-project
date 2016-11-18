35256ec2753aa0592e18938bcf4e2f15e746a7b6
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-11-18 10:04:51 +0100

    Update RippingLog.py

--- MyPythonProject/AudioCD/RippingLog.py
+++ MyPythonProject/AudioCD/RippingLog.py
@@ -1,4 +1,5 @@
 # -*- coding: ISO-8859-1 -*-
+import os
 import sys
 import argparse
 from Applications.Database.RippedCD.shared import insertfromfile
@@ -6,11 +7,21 @@ from Applications.Database.RippedCD.shared import insertfromfile
 __author__ = 'Xavier ROSSET'
 
 
+# ==========
+# Functions.
+# ==========
+def validdb(arg):
+    if not os.path.exists(arg):
+        raise argparse.ArgumentTypeError('"{0}" doesn\'t exist.'.format(arg))
+    return arg
+
+
 # =================
 # Arguments parser.
 # =================
 parser = argparse.ArgumentParser()
 parser.add_argument("tracks", type=argparse.FileType(mode="r"))
+parser.add_argument("-d", "--db", dest="database", default=os.path.join(os.path.expandvars("%_COMPUTING%"), "database.db"), type=validdb)
 
 
 # ================
@@ -22,4 +33,4 @@ arguments = parser.parse_args()
 # ===============
 # Main algorithm.
 # ===============
-sys.exit(insertfromfile(arguments.tracks))
+sys.exit(insertfromfile(arguments.tracks, db=arguments.database))
