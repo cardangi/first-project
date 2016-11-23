be4c09618c1eb4d242ec390e8fc8060f82fecf90
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-11-23 09:57:39 +0100

    Update DigitalAudioFiles`View3.py

--- MyPythonProject/Database/DigitalAudioFiles`View3.py
+++ MyPythonProject/Database/DigitalAudioFiles`View3.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import yaml
+import logging
 import argparse
 import datetime
+from logging.config import dictConfig
 from Applications.Database.DigitalAudioFiles.shared import select
 from Applications.shared import WRITE, LOCAL, TEMPLATE2, UTF8, dateformat
 
@@ -24,12 +27,29 @@ def thatfunc(d):
     return d
 
 
+def thatotherfunc(b):
+    if b:
+        return "debug"
+    return "info"
+
+
 # =================
 # Arguments parser.
 # =================
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--db", dest="database", default=os.path.join(os.path.expandvars("%_COMPUTING%"), "database.db"), type=validdb)
 parser.add_argument("-o", "--output", default=os.path.join(os.path.expandvars("%TEMP%"), "digitalaudiofiles.json"), type=argparse.FileType(mode=WRITE, encoding=UTF8))
+parser.add_argument("-u", "--debug", action="store_true")
+
+
+# ========
+# Logging.
+# ========
+with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
+    dictConfig(yaml.load(fp))
+logger = logging.getLogger("{1}.{0}".format(os.path.splitext(os.path.basename(__file__))[0], thatotherfunc(arguments.debug)))
+logger.debug(__file__)
+logger.info(__file__)
 
 
 # ================
