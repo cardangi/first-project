16686005d21009553dc33cb4c1b2a6951e499cae
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-11-28 22:17:08 +0100

    Update S09.py

--- MyPythonProject/Samples/S09.py
+++ MyPythonProject/Samples/S09.py
@@ -8,11 +8,22 @@ from Applications.shared import filesinfolder, zipfileparser
 __author__ = "Xavier ROSSET"
 
 
+# ========
+# Logging.
+# ========
 with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
     dictConfig(yaml.load(fp))
 logger = logging.getLogger("{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
 
+
+# ==========
+# Arguments.
+# ==========
 arguments = zipfileparser.parse_args()
 
+
+# ===============
+# Main algorithm.
+# ===============
 for fil in filesinfolder(arguments.source):
     logger.debug(fil)
