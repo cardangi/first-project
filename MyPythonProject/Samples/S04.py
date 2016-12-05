9b479f36f737ff37848c5bca224b1d2dd70b6fae
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-05 22:54:12 +0100

    Use Generator Expressions

--- MyPythonProject/Samples/S04.py
+++ MyPythonProject/Samples/S04.py
@@ -20,7 +20,7 @@ logger = logging.getLogger("{0}".format(os.path.splitext(os.path.basename(__file
 # ===============
 # Main algorithm.
 # ===============
-mylist = [("2014", "AB"), ("2010", "A"), ("2013", "B"), ("2010", "C"), ("2016", "D"), ("2015", "E"), ("2010", "F"), ("2016", "G"), ("2012", "H"), ("2012", "I"), ("2010", "J"), ("2014", "AA"),
-          ("2015", "K")]
+mylist = (("2014", "AB"), ("2010", "A"), ("2013", "B"), ("2010", "C"), ("2016", "D"), ("2015", "E"), ("2010", "F"), ("2016", "G"), ("2012", "H"), ("2012", "I"), ("2010", "J"), ("2014", "AA"),
+          ("2015", "K"))
 for k, v in groupby(sorted(sorted(mylist, key=itemgetter(1)), key=lambda i: int(i[0])), key=lambda i: int(i[0])):
     logger.debug("{0}: {1}".format(k, sorted([itemgetter(1)(item) for item in v])))
