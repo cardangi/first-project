e898c7d7a5b71528c13422ced12d217bcb7ae4d5
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-12-09 11:45:12 +0100

    Update Delete.py

--- MyPythonProject/AudioCD/Delete.py
+++ MyPythonProject/AudioCD/Delete.py
@@ -9,16 +9,27 @@ from Applications.Database.AudioCD.shared import deletefromuid
 __author__ = 'Xavier ROSSET'
 
 
-# ================
-# Initializations.
-# ================
-digits = r"\d(?:\d(?:\d(?:\d)?)?)?"
+lass SetExtensions(argparse.Action):
+    """
+    Set "uid" attribute.
+    """
+    rex1, rex2 = re.compile(r"\d(?:\d(?:\d(?:\d)?)?)?"), re.compile(r"^({0})\b\s?-\s?\b({0})$".format(digits))
+  
+    def __init__(self, option_strings, dest, **kwargs):
+        super(SetExtensions, self).__init__(option_strings, dest, **kwargs)
 
+    def __call__(self, parsobj, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+        
+        # Ranged Unique ID.
+        match = self.rex2.match(values)
+        if match:
+            setattr(namespace, "uid", list(range(int(match.group(1)), int(match.group(2)) + 1)))
 
-# ====================
-# Regular expressions.
-# ====================
-rex1, rex2 = re.compile(digits), re.compile(r"^({0})\b\s?-\s?\b({0})$".format(digits))
+        # Singled Unique ID.
+        uid = self.rex1.findall(values)
+        if uid:
+            setattr(namespace, "uid", list(map(int, uid)))
 
 
 # =================
