eec156c31d8942fe755ab7a7be6cfb83fec91464
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2016-12-16 13:43:48 +0100

    Update S11.py

--- MyPythonProject/Samples/S11.py
+++ MyPythonProject/Samples/S11.py
@@ -6,7 +6,7 @@ import logging
 from base64 import b85decode
 from logging.config import dictConfig
 from Applications.shared import PASSWORD
-from contextlib import ExitStack
+from contextlib import ExitStack, ContextDecorator
 
 __author__ = 'Xavier ROSSET'
 
@@ -14,7 +14,7 @@ __author__ = 'Xavier ROSSET'
 # ========
 # Classes.
 # ========
-class Toto(object):
+class ChangeCurrentDirectory(ContextDecorator):
 
     def __init__(self, ftpobj, dir):
         self.dir = dir
@@ -29,6 +29,22 @@ class Toto(object):
         self.ftpobj.cwd(self.cwd)
 
 
+# ==========
+# Functions.
+# ==========
+def foldercontent(dir, ftpobj):
+    for item in ftpobj.nlst(dir):
+        wdir = "{0}/{1}".format(dir, item)
+        stack2 = ExitStack()
+        try:
+            stack2.enter_context(ChangeCurrentDirectory(ftpobj, wdir))
+        except SomeError:
+            yield wdir
+        else:
+            with stack2:
+                foldercontent(wdir, ftpobj)
+
+
 # ========
 # Logging.
 # ========
@@ -40,31 +56,26 @@ logger = logging.getLogger("Default.{0}".format(os.path.splitext(os.path.basenam
 # ===============
 # Main algorithm.
 # ===============
-stack = ExitStack()
+stack1 = ExitStack()
 try:
-    ftp = stack.enter_context(ftplib.FTP(r"192.168.1.20", timeout=30))
+    ftp = stack1.enter_context(ftplib.FTP(r"192.168.1.20", timeout=30))
 except TimeoutError as err:
     logger.debug(err)
 else:
-    with stack:
+    with stack1:
         ftp.login(user="admin", passwd=b85decode(PASSWORD).decode())
         logger.debug(ftp.getwelcome())
-        while True:
-            try:
-                ftp.cwd("/music/S/Springsteen, Bruce")
-            except ftplib.error_perm:
-                ftp.mkd("/music/S/Springsteen, Bruce")
-                continue
-            break
-        ftp.storbinary(r'STOR {0}'.format(os.path.basename(r"F:\S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T06.flac")),
-                       open(r"F:\S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T06.flac", mode="rb"))
-        ftp.nlst()
-        ftp.cwd("/music")
-        for file in ftp.nlst():
+        # while True:
+            # try:
+                # ftp.cwd("/music/S/Springsteen, Bruce")
+            # except ftplib.error_perm:
+                # ftp.mkd("/music/S/Springsteen, Bruce")
+                # continue
+            # break
+        # ftp.storbinary(r'STOR {0}'.format(os.path.basename(r"F:\S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T06.flac")),
+                       # open(r"F:\S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T06.flac", mode="rb"))
+        # ftp.nlst()
+        refdirectory = "/music"
+        ftp.cwd(refdirectory)
+        for file in foldercontent(refdirectory, ftp):
             logger.debug(file)
-            with Toto(ftp, r"/music/{0}".format(file)):
-                for subfile in ftp.nlst():
-                    logger.debug(subfile)
-                    with Toto(ftp, r"/music/{0}/{1}".format(file, subfile)):
-                        for subsubfile in ftp.nlst():
-                            logger.debug(subsubfile)
