15bc5121e8273e711693c78c4d15e95777b342a5
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-17 12:19:07 +0100

    Save changes

--- MyPythonProject/Samples/S11.py
+++ MyPythonProject/Samples/S11.py
@@ -14,13 +14,13 @@ __author__ = 'Xavier ROSSET'
 # ==========
 # Functions.
 # ==========
-def directorycontent(ftpobject, currentdir, logobj=None):
+def directorycontent(ftpobject, currentdir, logobject=None):
     for item in ftpobject.nlst():
         wdir = "{0}/{1}".format(currentdir, item)
-        if logobj:
-            logobj.debug(item)
-            logobj.debug(currentdir)
-            logobj.debug(wdir)
+        if logobject:
+            logobject.debug(currentdir)
+            logobject.debug(item)
+            logobject.debug(wdir)
         stack2 = ExitStack()
         try:
             stack2.enter_context(ChgCurDir(ftpobject, wdir))
@@ -28,37 +28,37 @@ def directorycontent(ftpobject, currentdir, logobj=None):
             yield wdir
         else:
             with stack2:
-                for content in directorycontent(ftpobject=ftpobject, currentdir=wdir, logobj=logobj):
+                for content in directorycontent(ftpobject=ftpobject, currentdir=wdir, logobject=logobject):
                     yield content
-
-
-# ========
-# Logging.
-# ========
-with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
-    dictConfig(yaml.load(fp))
-logger = logging.getLogger("Default.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
 
 
 # ===============
 # Main algorithm.
 # ===============
-stack1 = ExitStack()
-try:
-    ftp = stack1.enter_context(ftplib.FTP(NAS, timeout=30))
-except TimeoutError as err:
-    logger.exception(err)
-else:
-    with stack1:
-        ftp.login(user="admin", passwd=b85decode(PASSWORD).decode())
-        logger.debug(ftp.getwelcome())
-        refdirectory = "/music"
-        try:
-            ftp.cwd(refdirectory)
-        except ftplib.error_perm as err:
-            logger.exception(err)
-        else:
-            logger.debug("Current directory before: {0}.".format(ftp.pwd()))
-            for file in directorycontent(ftpobject=ftp, currentdir=refdirectory):
-                logger.debug(file)
-            logger.debug("Current directory after: {0}.".format(ftp.pwd()))
+if __name__ == "__main__":
+
+    # --> Logging.
+    with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
+        dictConfig(yaml.load(fp))
+    logger = logging.getLogger("Default.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
+
+    # --> Main alogrithm.
+    stack1 = ExitStack()
+    try:
+        ftp = stack1.enter_context(ftplib.FTP(NAS, timeout=30))
+    except TimeoutError as err:
+        logger.exception(err)
+    else:
+        with stack1:
+            ftp.login(user="admin", passwd=b85decode(PASSWORD).decode())
+            logger.debug(ftp.getwelcome())
+            refdirectory = "/music"
+            try:
+                ftp.cwd(refdirectory)
+            except ftplib.error_perm as err:
+                logger.exception(err)
+            else:
+                logger.debug("Current directory before: {0}.".format(ftp.pwd()))
+                for file in directorycontent(ftpobject=ftp, currentdir=refdirectory):
+                    logger.debug(file)
+                logger.debug("Current directory after: {0}.".format(ftp.pwd()))
--- MyPythonProject/Samples/S15.py
+++ MyPythonProject/Samples/S15.py
@@ -1,71 +1,54 @@
 # -*- coding: utf-8 -*-
 import os
+import json
 import yaml
 import ftplib
 import logging
 from base64 import b85decode
+from contextlib import ExitStack
 from logging.config import dictConfig
-from Applications.shared import PASSWORD
-from contextlib import ExitStack, ContextDecorator
+from Applications.shared import NAS, PASSWORD, ChgCurDir
+from Samples.S11 import directorycontent
 
 __author__ = 'Xavier ROSSET'
-
-
-# ========
-# Classes.
-# ========
-class ChangeCurrentDirectory(ContextDecorator):
-
-    def __init__(self, ftpobj, dir):
-        self.dir = dir
-        self.ftpobj = ftpobj
-        self.cwd = ftpobj.pwd()
-
-    def __enter__(self):
-        self.ftpobj.cwd(self.dir)
-        return self
-
-    def __exit__(self, *exc):
-        self.ftpobj.cwd(self.cwd)
-
-
-# ========
-# Logging.
-# ========
-with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
-    dictConfig(yaml.load(fp))
-logger = logging.getLogger("Default.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
 
 
 # ===============
 # Main algorithm.
 # ===============
-# JSON avec ["S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T01.flac", "S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T02.flac"]
-# On extrait les deux premiers répertoires et le tag "albumsort" pour créer une variable "S\Springsteen, Bruce\2.20160914.1".
-with open(myfile) as fp:
+if __name__ == "__main__":
 
-    stack1 = ExitStack()
-    try:
-        ftp = stack1.enter_context(ftplib.FTP(r"192.168.1.20", timeout=30))
-    except TimeoutError as err:
-        logger.exception(err)
-    else:
-        with stack1:
-            ftp.login(user="admin", passwd=b85decode(PASSWORD).decode())
-            logger.debug(ftp.getwelcome())
-            refdirectory = "/music"
-            ftp.cwd(refdirectory)
-            for item in json.load(fp):
-                wdir = "{0}/{1}".format(refdirectory, output.replace("\\", "/"))  # "S/Springsteen, Bruce/2.20160914.1".
-                stack2 = ExitStack()
-                while True:
-                    try:
-                        stack2.enter_context(ChangeCurrentDirectory(ftp, wdir))
-                    except ftplib.error_perm:
-                        logger.debug('"{0}" created.'.format(wdir))
-                        ftp.mkd(wdir)
-                        continue
-                    else:
-                        with stack2:
-                            ftp.storbinary(r"STOR {0}".format(os.path.basename(item))), open(item, mode="rb"))
-                        break
+    # --> Logging.
+    with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
+        dictConfig(yaml.load(fp))
+    logger = logging.getLogger("Default.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
+
+    # JSON avec ["S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T01.flac", "S\Springsteen, Bruce\2\2016\09.14 - Foxboro,  MA\CD4\1.Free Lossless Audio Codec\2.20160914.1.13.D4.T02.flac"]
+    # On extrait les deux premiers répertoires et le tag "albumsort" pour créer une variable "S\Springsteen, Bruce\2.20160914.1".
+    with open(myfile) as fp:
+
+        stack1 = ExitStack()
+        try:
+            ftp = stack1.enter_context(ftplib.FTP(NAS, timeout=30))
+        except TimeoutError as err:
+            logger.exception(err)
+        else:
+            with stack1:
+                ftp.login(user="admin", passwd=b85decode(PASSWORD).decode())
+                logger.debug(ftp.getwelcome())
+                refdirectory = "/music"
+                ftp.cwd(refdirectory)
+                for item in json.load(fp):
+                    wdir = "{0}/{1}".format(refdirectory, output.replace("\\", "/"))  # "S/Springsteen, Bruce/2.20160914.1".
+                    stack2 = ExitStack()
+                    while True:
+                        try:
+                            stack2.enter_context(ChgCurDir(ftp, wdir))
+                        except ftplib.error_perm:
+                            logger.debug('"{0}" created.'.format(wdir))
+                            ftp.mkd(wdir)
+                            continue
+                        else:
+                            with stack2:
+                                ftp.storbinary(r"STOR {0}".format(os.path.basename(item)), open(item, mode="rb"))
+                            break
