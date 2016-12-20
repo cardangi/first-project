b9395d2ac85f7c97edf6c7d50abe696e50983190
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-21 00:50:31 +0100

    Some tests with timeit module

--- MyPythonProject/Samples/S18.py
+++ MyPythonProject/Samples/S18.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from timeit import timeit
 
-__author__ == "Xavier ROSSET"
+__author__ = "Xavier ROSSET"
 
 imp = """\
 import os
 from Applications.shared import filesinfolder
 """
-print(timeit(stmt='filesinfolder("jpg", folder=r"G:\Vidéos\Samsung S5")', setup='from Applications.shared import filesinfolder', number=10000))
-print(timeit(stmt='sorted(sorted(filesinfolder("jpg", folder=r"G:\Vidéos\Samsung S5")), key=lambda i: os.path.splitext(i)[1][1:])', setup=imp, number=10000))
+print(timeit(stmt='filesinfolder("jpg", folder=r"G:\Vidéos\Samsung S5")', setup='from Applications.shared import filesinfolder', number=1000000))
+print(timeit(stmt='sorted(sorted(filesinfolder("jpg", folder=r"G:\Vidéos\Samsung S5")), key=lambda i: os.path.splitext(i)[1][1:])', setup=imp, number=1000000))
