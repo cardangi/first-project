e569dc5236902a7db5619172738f5b3b351fff11
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-27 16:26:48 +0100

    New python scripts for new tables

--- MyPythonProject/Applications/Database/Tables/shared.py
+++ MyPythonProject/Applications/Database/Tables/shared.py
@@ -35,9 +35,9 @@ def selectfromuid(uid, table, db=DATABASE):
     conn.row_factory = sqlite3.Row
     for row in conn.execute("SELECT * FROM {0} WHERE rowid=?".format(table), (uid,)):
         logger = logging.getLogger("{0}.selectfromuid".format(__name__))
-        logger.debug("Table          : {0}".format(table))
-        logger.debug("Selected record:")
-        logger.debug("Unique ID: {0:>4d}.".format(uid))
+        logger.debug("Database: {0}.".format(db))
+        logger.debug("Table   : {0}.".format(table))
+        logger.debug("ID      : {0:>3d}.".format(uid))
         for item in tuple(row):
             logger.debug("\t{0}".format(item).expandtabs(3))
         yield tuple(row)
@@ -55,15 +55,17 @@ def insert(*uid, db=DATABASE, table=None, date=None):
         conn.executemany("INSERT INTO {0} (id, {1}) VALUES(?, ?)".format(table, MAPPING[table]), zip(uid, repeat(date)))
         status = conn.total_changes
         logger = logging.getLogger("{0}.insert".format(__name__))
-        logger.debug("Table: {0}".format(table))
-        logger.debug("{0} records inserted.".format(status))
+        logger.debug("Database: {0}.".format(db))
+        logger.debug("Table   : {0}.".format(table))
+        logger.debug("{0:>3d} records inserted.".format(status))
         if status:
             for item in uid:
-                logger.debug("Unique ID: {0:>4d}.".format(item))
+                logger.debug("ID      : {0:>3d}.".format(item))
     return status
 
 
 def update(uid, db=DATABASE, table=None, date=None):
+
     if table is None:
         return 0
     if table not in MAPPING:
@@ -72,18 +74,12 @@ def update(uid, db=DATABASE, table=None, date=None):
         date = datetime.datetime.utcnow()
     recordid = list(selectfromuid(uid, table, db))
 
-    # Record still exists: it is updated.
+    # Record exists: it is updated.
     if recordid:
-        conn = sqlite3.connect(db)
+        status, conn = 0, sqlite3.connect(db)
         with conn:
             conn.execute("UPDATE {0} SET {1}=? WHERE id=?".format(table, MAPPING[table]), (date, uid))
             status = conn.total_changes
-            # logger = logging.getLogger("{0}.insert".format(__name__))
-            # logger.debug("Table: {0}".format(table))
-            # logger.debug("{0} records inserted.".format(status))
-            # if status:
-            #     for item in uid:
-            #         logger.debug("Unique ID: {0:>4d}.".format(item))
         return status
 
     # Record doesn't exist: it is inserted.
@@ -100,11 +96,12 @@ def deletefromuid(*uid, db=DATABASE, table=None):
         conn.executemany("DELETE FROM {0} WHERE id=?".format(table), [(i,) for i in uid])
         status = conn.total_changes
         logger = logging.getLogger("{0}.deletefromuid".format(__name__))
-        logger.debug("Table: {0}".format(table))
-        logger.debug("{0} records removed.".format(status))
+        logger.debug("Database: {0}.".format(db))
+        logger.debug("Table   : {0}.".format(table))
+        logger.debug("{0:>3d} records removed.".format(status))
         if status:
             for item in uid:
-                logger.debug("Unique ID: {0:>4d}.".format(item))
+                logger.debug("ID      : {0:>3d}.".format(item))
     return status
 
 
@@ -118,9 +115,7 @@ def delete(db=DATABASE, table=None):
         conn.execute("DELETE FROM {0}".format(table))
         status = conn.total_changes
         logger = logging.getLogger("{0}.delete".format(__name__))
-        logger.debug("Table: {0}".format(table))
-        logger.debug("{0} records removed.".format(status))
+        logger.debug("Database: {0}.".format(db))
+        logger.debug("Table   : {0}.".format(table))
+        logger.debug("{0:>3d} records removed.".format(status))
     return status
-
-# AAAAAAAAAAAAAAAAAAAAAAAAAA
-# BBBBBBBBBBBBBBBBBBBBBBBBBB
