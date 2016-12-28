8a4e0d6b731ae195ce6e7ad3c188c47d43dd59d7
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-28 21:19:17 +0100

    Some optimizations

--- MyPythonProject/Samples/S20.py
+++ MyPythonProject/Samples/S20.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
+import yaml
+import logging
+from logging.config import dictConfig
+from subprocess import run
 from Applications.parsers import readtable
-from Applications.Database.Tables.shared import isdeltareached
+from Applications.Database.Tables.shared import isdeltareached, update
 
 __author__ = 'Xavier ROSSET'
+
+
+# ========
+# Logging.
+# ========
+with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
+    dictConfig(yaml.load(fp))
+logger = logging.getLogger("Database.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
 
 
 # ==========
@@ -22,6 +35,11 @@ arguments = readtable.parse_args()
 # ===============
 # Main algorithm.
 # ===============
-deltareached = MAPPING[isdeltareached(arguments.uid, arguments.table, arguments.database, arguments.delta)]
-print(deltareached)
-sys.exit(deltareached)
+status = 0
+if isdeltareached(arguments.uid, arguments.table, arguments.database, arguments.delta):
+    process = run(["C:\Program Files\Sandboxie\Start.exe", "/box:GNUCash", "delete_sandbox_silent"])
+    logger.debug(process.returncode)
+    if not process.returncode:
+        status = update(arguments.uid, db=arguments.database, table=arguments.table)
+logger.debug(status)
+sys.exit(status)
