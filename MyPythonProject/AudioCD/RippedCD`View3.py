d15f7d514ab03b5411a583f8e851cef871a62c4e
Xavier <xavier.cpvx-rosset@outlook.fr>
2016-12-29 03:27:03 +0100

    Some improvements

--- MyPythonProject/AudioCD/RippedCD`View3.py
+++ MyPythonProject/AudioCD/RippedCD`View3.py
@@ -8,7 +8,15 @@ import os
 __author__ = 'Xavier ROSSET'
 
 
+# ========
+# Logging.
+# ========
 with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding=UTF8) as fp:
     dictConfig(yaml.load(fp))
+
+
+# ===============
+# Main algorithm.
+# ===============
 for row in select():
     print(row)
