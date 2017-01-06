3ea36ed1b40c2a596de877796b692d3481b94223
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2017-01-06 15:35:38 +0100

    Update DigitalAudioFiles`CopyArguments.py

--- MyPythonProject/AudioCD/DigitalAudioFiles`CopyArguments.py
+++ MyPythonProject/AudioCD/DigitalAudioFiles`CopyArguments.py
@@ -66,7 +66,7 @@ rex2 = re.compile(r"[a-z]:", re.IGNORECASE)
 # ========
 with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
     dictConfig(yaml.load(fp))
-logger = logging.getLogger("AudioCD.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
+logger = logging.getLogger("Applications.AudioCD")
 
 
 # ===============
