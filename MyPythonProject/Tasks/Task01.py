c345d08500814887bdd54b76c8536f8999f1cd2d
cestMoiBaliBalo <xavier.cpvx-rosset@outlook.fr>
2017-01-06 15:04:53 +0100

    Update Task01.py

--- MyPythonProject/Tasks/Task01.py
+++ MyPythonProject/Tasks/Task01.py
@@ -4,20 +4,13 @@ import sys
 import yaml
 import logging
 import argparse
+import itertools
 from subprocess import run
 from logging.config import dictConfig
 from Applications.shared import validdb, DATABASE
 from Applications.Database.Tables.shared import isdeltareached, update
 
 __author__ = 'Xavier ROSSET'
-
-
-# ========
-# Logging.
-# ========
-with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
-    dictConfig(yaml.load(fp))
-logger = logging.getLogger("Tables.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
 
 
 # ==========
@@ -27,26 +20,31 @@ parser = argparse.ArgumentParser()
 parser.add_argument("database", nargs="?", default=DATABASE, type=validdb, help="Read/Updated database")
 
 
-# ==========
-# CONSTANTS.
-# ==========
-UID = 123456798
-TABLE = "rundates"
-
-
-# ================
-# Initializations.
-# ================
-status, arguments = 0, parser.parse_args()
-
-
 # ===============
 # Main algorithm.
 # ===============
-if isdeltareached(UID, TABLE, arguments.database):
-    process = run(["C:\Program Files\Sandboxie\Start.exe", "/box:GNUCash", "delete_sandbox_silent"])
-    logger.debug(process.returncode)
-    if not process.returncode:
-        status = update(UID, TABLE, arguments.database)
-logger.debug(status)
-sys.exit(status)
+if __name__ == "__main__":
+
+    #  1. --> Logging.
+    with open(os.path.join(os.path.expandvars("%_COMPUTING%"), "logging.yml"), encoding="UTF_8") as fp:
+        dictConfig(yaml.load(fp))
+    logger = logging.getLogger("Tables.{0}".format(os.path.splitext(os.path.basename(__file__))[0]))
+
+    #  2. --> Initializations.
+    UID = 123456798
+
+    #  3. --> Initializations.
+    status, arguments = 0, parser.parse_args()
+    isdeltareached = functools.partial(isdeltareached, UID, "rundates")
+    update = functools.partial(update, UID, "rundates")
+
+    #  4. --> Main algorithm.
+    if isdeltareached(arguments.database):
+        process = run(["C:\Program Files\Sandboxie\Start.exe", "/box:GNUCash", "delete_sandbox_silent"])
+        logger.debug(process.returncode)
+        if not process.returncode:
+            status = update(arguments.database)
+
+    #  5. --> Exit algorithm.
+    logger.debug(status)
+    sys.exit(status)
