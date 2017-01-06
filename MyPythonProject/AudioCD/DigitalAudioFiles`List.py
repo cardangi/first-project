e839a5c0edf7b157c5d01ded013079b2f170e442
Xavier <xavier.cpvx-rosset@outlook.fr>
2017-01-06 20:07:50 +0100

    Optimizations

--- MyPythonProject/AudioCD/DigitalAudioFiles`List.py
+++ MyPythonProject/AudioCD/DigitalAudioFiles`List.py
@@ -12,6 +12,7 @@ from Applications import shared
 from os.path import normpath, splitext
 from sortedcontainers import SortedDict
 import xml.etree.ElementTree as ElementTree
+from Applications.descriptors import Folder, Extensions
 
 __author__ = 'Xavier ROSSET'
 
@@ -25,79 +26,23 @@ locale.setlocale(locale.LC_ALL, "")
 # ========
 # Classes.
 # ========
-class Interface(object):
+class LocalInterface(shared.GlobalInterface):
 
-        _regex = re.compile(r"\W+")
-        _inputs = [("Please enter directory to walk through", "directory"),
-                   ("Please enter extension(s) to filter out", "extensions")]
+    # Data descriptor(s).
+    folder = Folder()
+    extensions = Extensions()
 
-    def __init__(self):
-        self._index, self._step = 0, 0
-        self._extensions = None
-        self._directory = None
-        self._arguments = []
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._index >= len(self._inputs):
-            raise StopIteration
-        self._index += 1
-        self._step += 1
-        return self._inputs[self._index - 1]
-
-    # -----
-    # STEP.
-    # -----
-    @property
-    def step(self):
-        return self._step
-
-    # ----------
-    # ARGUMENTS.
-    # ----------
-    @property
-    def arguments(self):
-        return self._arguments
-
-    # ----------
-    # DIRECTORY.
-    # ----------
-    @property
-    def directory(self):
-        return self._directory
-
-    @directory.setter
-    def directory(self, arg):
-        if not os.path.isdir(arg):
-            raise ValueError('"{0}" is not a valid directory'.format(arg))
-        if not os.access(d, os.R_OK):
-            raise ValueError('"{0}" is not a readable directory'.format(arg))
-        self._directory = arg
-        self._arguments.append(arg)
-
-    # -----------
-    # EXTENSIONS.
-    # -----------
-    @property
-    def extensions(self):
-        return self._extensions
-
-    @extensions.setter
-    def extensions(self, arg):
-        if arg:
-            self._extensions = arg
-            self._arguments.extend(self._regex.split(arg))
+    # Instance method(s).
+    def __init__(self, *args):
+        super(LocalInterface, self).__init__(*args)
 
 
 # =================
 # Arguments parser.
 # =================
 parser = argparse.ArgumentParser()
-parser.add_argument("directory", help="mandatory directory to walk through", type=isvaliddirectory)
+parser.add_argument("directory", help="mandatory directory to walk through", type=shared.validpath)
 parser.add_argument("extensions", help="one or more extension(s) to filter out", nargs="*")
-arguments = parser.parse_args()
 
 
 # ==========
@@ -111,20 +56,20 @@ OUTFILE = os.path.join(os.path.expandvars("%TEMP%"), "ranking.json")
 # ===============
 if __name__ == "__main__":
 
-
     #     ---------------- 
     #  0. Initializations.
     #     ---------------- 
     reflist, lista, listb, listc, listd, liste, ext_list, art_list, artext_dict, extensions, artists = [], None, None, None, None, None, [], [], SortedDict(), SortedDict(), SortedDict()
     rex1 = re.compile(r"^(?:[^\\]+\\){2}([^\\]+)\\")
-
+    arguments = []
 
     # --> User interface.
-    gui = interface(Interface())
+    gui = shared.interface(LocalInterface([("Please enter directory to walk through", "folder"), ("Please enter extension(s) to filter out", "extensions")]))
 
     # --> Parse arguments.
-    arguments = parser.parse_args(gui.arguments)
-
+    arguments.append(gui.folder)
+    arguments.extend(gui.extensions)
+    arguments = parser.parse_args(arguments)
 
     #     ------------------------
     #  1. Inventaire des fichiers.
@@ -144,11 +89,12 @@ if __name__ == "__main__":
                 artext_dict[art] = list()
             artext_dict[art].append(ext)
 
-
     #     --------------------
     #  2. Total par extension.
     #     --------------------
-    ext_count = collections.Counter(ext_list)
+    ext_count = collections.Counter()
+    for extension in ext_list:
+        ext_count[extension] += 1
 
     #  Tri par nom croissant.
     ext_count1 = collections.OrderedDict(sorted(ext_count.items(), key=itemgetter(0)))
@@ -156,11 +102,12 @@ if __name__ == "__main__":
     #  Tri par total décroissant et par nom croissant.
     ext_count2 = collections.OrderedDict(sorted(sorted(ext_count.items(), key=itemgetter(0)), key=itemgetter(1), reverse=True))
 
-
     #     ------------------
     #  3. Total par artiste.
     #     ------------------
-    art_count = collections.Counter(art_list)
+    art_count = collections.Counter()
+    for artist in art_list:
+        art_count[artist] += 1
 
     #  Tri par nom croissant.
     art_count1 = collections.OrderedDict(sorted(art_count.items(), key=itemgetter(0)))
@@ -168,13 +115,14 @@ if __name__ == "__main__":
     #  Tri par total décroissant et par nom croissant.
     art_count2 = collections.OrderedDict(sorted(sorted(art_count.items(), key=itemgetter(0)), key=itemgetter(1), reverse=True))
 
-
     #     -----------------------------------
     #  4. Total par couple artiste/extension.
     #     -----------------------------------
     for artist in artext_dict:
-        artext_dict[artist] = collections.OrderedDict(sorted(collections.Counter(artext_dict[artist]).items(), key=itemgetter(0)))
-
+        count = collections.Counter()
+        for extension in artext_dict[artist]:
+            count[extension] += 1
+        artext_dict[artist] = collections.OrderedDict(sorted(count.items(), key=itemgetter(0)))
 
     #     ------
     #  4. Files.
@@ -187,13 +135,11 @@ if __name__ == "__main__":
         # ----- Liste des 50 fichiers créés dernièrement. Tri par date décroissante, puis nom croissant.
         listb = ((a, b, c) for a, (b, c) in enumerate([(itemgetter(0)(item), itemgetter(2)(item)) for item in sorted(sorted(reflist, key=itemgetter(0)), key=itemgetter(1), reverse=True)[:50]], 1))
 
-
     #     -----------
     #  5. Extensions.
     #     -----------
     if extensions:
         listc = ((a, b, c) for a, (b, c) in enumerate([(itemgetter(0)(item), itemgetter(1)(item)) for item in sorted(extensions.items(), key=itemgetter(0))], 1))
-
 
     #     --------
     #  6. Artists.
@@ -205,7 +151,6 @@ if __name__ == "__main__":
 
         # ----- Liste des artistes. Tri par ranking décroissant, puis nom croissant.
         liste = ((a, b, c) for a, (b, c) in enumerate([(itemgetter(0)(item), itemgetter(1)(item)) for item in sorted(sorted(artists.items(), key=itemgetter(0)), key=itemgetter(1), reverse=True)], 1))
-
 
     #     -----------
     #  7. XML Output.
@@ -240,7 +185,6 @@ if __name__ == "__main__":
             file.text = item2
     if any([lista, listb, listc, listd, liste]):
         ElementTree.ElementTree(root).write(os.path.join(os.path.expandvars("%_COMPUTING%"), "DigitalAudioFilesList.xml"), encoding="UTF-8", xml_declaration=True)
-
 
     #     ---------------
     #  8. Ranking Output.
